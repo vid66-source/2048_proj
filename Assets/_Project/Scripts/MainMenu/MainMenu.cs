21903617e4bf82dce8ea3707c2eef96169d453ea
vid66 <v.khristoliubov@red-beat.com>
2026-05-31 22:41:05 +0300

    Trivial Changes

---
+++ Assets/_Project/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void LoadGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
